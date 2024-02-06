bcd3017c2dfd30f2b1f892a499434ebbec20de86
SamuelGalantei4sea <samuel.galante@i4sea.com>
2024-02-06 16:41:53 -0300

    🎉 INT COMMIT: Initial list with drag and drop

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Card } from "./Card";
+
+/* Regra de compressão
+  'aaaabbccc' -> 'a4b2c3' 
+  'abbbc' -> 'a1b3c1'
+*/
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      title: "aaaabbccc",
+    },
+    {
+      id: 2,
+      title: "abbbc",
+    },
+    {
+      id: 3,
+      title: "aaccdd",
+    },
+  ]);
+
+  const moveItem = (dragIndex: number, hoverIndex: number) => {
+    const dragItem = items && items[dragIndex];
+
+    setItems((prevState: any) => {
+      const itemsList = [...prevState];
+      const prevItem = itemsList.splice(hoverIndex, 1, dragItem);
+      itemsList.splice(dragIndex, 1, prevItem[0]);
+
+      return itemsList;
+    });
+  };
+
+  const renderCard = () => {
+    return items.map((task: any, index: any) => (
+      <Card index={index} item={task} moveRow={moveItem} />
+    ));
+  };
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      {/* Adicione aqui o campo para inserir um novo item seguindo a regra de compressão */}
+      {renderCard()}
+    </DndProvider>
+  );
+}
+
+export default App;
---
+++ src/Card.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
+
+export function Card(props: any) {
+  const { id, title } = props.item;
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  const [collectedProps, drop] = useDrop({
+    accept: "dnd-items",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item: any, monitor: any) {
+      if (!ref.current) {
+        return;
+      }
+
+      const dragIndex = item.index;
+      const hoverIndex = props.index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      const clientOffset = monitor.getClientOffset();
+
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      props.moveRow(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [collectedDragProps, drag] = useDrag({
+    type: "dnd-items",
+    item: () => {
+      return { id, index: props.index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  const bgColor = collectedDragProps.isDragging ? "gray" : "";
+
+  return (
+    <div
+      style={{ backgroundColor: bgColor }}
+      ref={ref}
+      data-handler-id={collectedProps.handlerId}
+    >
+      <h1>{title}</h1>
+    </div>
+  );
+}
